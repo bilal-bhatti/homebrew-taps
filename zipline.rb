fae1336828c2b1c179f0d24045133dc04c5f0ad6
goreleaserbot <goreleaser@carlosbecker.com>
2022-03-27 18:34:58 -0500

    Brew formula update for zipline version v0.3.4

--- zipline.rb
+++ zipline.rb
@@ -5,20 +5,20 @@
 class Zipline < Formula
   desc ""
   homepage ""
-  version "0.3.3"
+  version "0.3.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/bilal-bhatti/zipline/releases/download/v0.3.3/zipline_0.3.3_Darwin_arm64.tar.gz"
-      sha256 "d93db4bac78c0aff8ee268ccfdbda897b5ba657778d0d9510233e0d76eee648f"
+      url "https://github.com/bilal-bhatti/zipline/releases/download/v0.3.4/zipline_0.3.4_Darwin_arm64.tar.gz"
+      sha256 "2c2596da32af652b0c4452e00d646127723056a1b9eaba5631044138ac051ac2"
 
       def install
         bin.install "zipline"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/bilal-bhatti/zipline/releases/download/v0.3.3/zipline_0.3.3_Darwin_x86_64.tar.gz"
-      sha256 "93ec1d9af494a9f71add9caea798db3d89f50f9ac176a6c2cffe264e2de24555"
+      url "https://github.com/bilal-bhatti/zipline/releases/download/v0.3.4/zipline_0.3.4_Darwin_x86_64.tar.gz"
+      sha256 "61568cc23facd9d5efe5307568a6e72cbbfff20f414b1f8dc8a822d528416c08"
 
       def install
         bin.install "zipline"
@@ -28,16 +28,16 @@ class Zipline < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/bilal-bhatti/zipline/releases/download/v0.3.3/zipline_0.3.3_Linux_arm64.tar.gz"
-      sha256 "1aeb8bc441824d62afa675c3748eb3906d2f8b92322e36c7ee2d8d13e45145ed"
+      url "https://github.com/bilal-bhatti/zipline/releases/download/v0.3.4/zipline_0.3.4_Linux_arm64.tar.gz"
+      sha256 "194f5c34e200fe6b84c68929b6bad3a24cff0b950c81d75a7237e898b126897e"
 
       def install
         bin.install "zipline"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/bilal-bhatti/zipline/releases/download/v0.3.3/zipline_0.3.3_Linux_x86_64.tar.gz"
-      sha256 "b64b9117a3b6453ec3c9b400e7dd956549e8dec3cb4fb41498545d38c1d7309f"
+      url "https://github.com/bilal-bhatti/zipline/releases/download/v0.3.4/zipline_0.3.4_Linux_x86_64.tar.gz"
+      sha256 "8a9957e0ced86f4e9f1f75b4500a50826644770487e29fd8d2cedbb67b63abc2"
 
       def install
         bin.install "zipline"
